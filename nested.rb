6ccc046561486c5b3264cab1f38040aaee60d53b
Kyle Rosipko <krosipko@outlook.com>
2019-03-02 23:08:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -127,6 +127,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-  programmer_hash[:dennis_ritchie][:known_for] = programmer_hash[:dennis_ritchie][:known_for], "Ass"
+  programmer_hash[:dennis_ritchie][:known_for] = programmer_hash[:dennis_ritchie][:known_for], "Assembly"
+  programmer_hash
 
 end
